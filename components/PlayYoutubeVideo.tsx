b6ff63091681a4a76ca74e7c135636c7295b24a7
shahriar <shohan.ndc@gmail.com>
2024-05-27 12:28:27 +0600

    first commit

---
+++ components/PlayYoutubeVideo.tsx
@@ -0,0 +1,34 @@
+import React, { Dispatch, SetStateAction, useCallback } from "react";
+import YoutubePlayer from "react-native-youtube-iframe";
+
+type PlayYoutubeVideoProps = {
+  videoId: string;
+  playing: boolean;
+  setPlaying: Dispatch<SetStateAction<boolean>>;
+};
+
+const PlayYoutubeVideo = ({
+  videoId,
+  playing,
+  setPlaying,
+}: PlayYoutubeVideoProps) => {
+  const onStateChange = useCallback(
+    (state: string) => {
+      if (state === "ended") {
+        setPlaying(false);
+      }
+    },
+    [setPlaying]
+  );
+
+  return (
+    <YoutubePlayer
+      height={200}
+      play={playing}
+      videoId={videoId}
+      onChangeState={onStateChange}
+    />
+  );
+};
+
+export default PlayYoutubeVideo;
---
+++ components/VideoPlay.tsx
@@ -0,0 +1,34 @@
+import React, { Dispatch, SetStateAction } from "react";
+import { Vimeo } from "react-native-vimeo-iframe";
+
+type PlayVimeoVideoProps = {
+  videoId: string;
+  playing: boolean;
+  setPlaying: Dispatch<SetStateAction<boolean>>;
+};
+
+const PlayVimeoVideo = ({
+  videoId,
+  playing,
+  setPlaying,
+}: PlayVimeoVideoProps) => {
+  console.log(videoId);
+  const videoCallbacks = {
+    play: (data: object) => console.log("play: ", data),
+    pause: () => setPlaying(!playing),
+    fullscreenchange: (data: object) => console.log("fullscreenchange: ", data),
+    ended: (data: object) => console.log("ended: ", data),
+    controlschange: (data: object) => console.log("controlschange: ", data),
+  };
+
+  return (
+    <Vimeo
+      videoId={"712158285"}
+      params={"api=1&autoplay=0"}
+      handlers={videoCallbacks}
+      style={{ height: 200 }}
+    />
+  );
+};
+
+export default PlayVimeoVideo;
