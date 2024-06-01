1b1d72801ef3fcec92195a571f94e6983cb615ed
abuabdullah23 <abuobaidazayed@gmail.com>
2024-06-01 16:42:35 +0600

    Delte unnecessary folder

--- components/radio/hooks/useRadioValue.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export default function useRadioValue() {
-  const [selected, setSelected] = useState<string | null>(null);
-
-  return { selected, setSelected };
-}
