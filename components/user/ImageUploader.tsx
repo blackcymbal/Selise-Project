42cee0c5f28e140083c4c781f6770747b04aa50f
shahriar <shohan.ndc@gmail.com>
2024-06-06 00:15:45 +0600

    take photo camera permisson added

--- components/user/ImageUploader.tsx
+++ components/user/ImageUploader.tsx
@@ -1,26 +1,66 @@
 import { ImageUploadIcon, User } from "@/assets/icons/icons";
 import theme from "@/constants/theme";
 import { fallbackImages } from "@/utils";
+import Constants from "expo-constants";
 import * as ImagePicker from "expo-image-picker";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
 import { Typography } from "../ui";
 
 export default function ImageUploader() {
   const [image, setImage] = useState("");
 
-  const pickImageAsync = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
+  useEffect(() => {
+    (async () => {
+      if (Constants?.platform?.ios) {
+        const cameraRollStatus =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+        if (
+          cameraRollStatus.status !== "granted" ||
+          cameraStatus.status !== "granted"
+        ) {
+          alert("Sorry, we need these permissions to make this work!");
+        }
+      }
+    })();
+  }, []);
+
+  const takePhoto = async () => {
+    let result = await ImagePicker.launchCameraAsync({
       allowsEditing: true,
       quality: 1,
     });
 
+    handleImagePicked(result);
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    handleImagePicked(result);
+  };
+
+  const handleImagePicked = async (result: ImagePicker.ImagePickerResult) => {
     if (!result.canceled) {
+      console.log(result);
       setImage(result.assets[0].uri);
     } else {
       alert("You did not select any image.");
     }
   };
+
+  // const formData = new FormData();
+  //     formData.append("file", {
+  //       name: "photo.jpg",
+  //       uri: result.assets[0].uri,
+  //       mime: result.assets[0].mimeType,
+  //       type: result.assets[0].type,
+  //     });
 
   return (
     <View style={styles.container}>
@@ -36,10 +76,7 @@ export default function ImageUploader() {
           <User color={theme.colors.gray500} height={36} width={36} />
         </View>
       )}
-      <TouchableOpacity
-        onPress={pickImageAsync}
-        style={styles.profilePicUploadBtn}
-      >
+      <TouchableOpacity onPress={pickImage} style={styles.profilePicUploadBtn}>
         <ImageUploadIcon
           width={20}
           height={20}
@@ -48,6 +85,9 @@ export default function ImageUploader() {
         <Typography weight="semiBold" color="primary700">
           ছবি আপলোড করুন
         </Typography>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={takePhoto}>
+        <Typography>Take Photo from camera</Typography>
       </TouchableOpacity>
     </View>
   );
