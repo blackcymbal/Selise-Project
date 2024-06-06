01281e8a205e570fe67e4c5abd20716aabbd510e
abuabdullah23 <abuobaidazayed@gmail.com>
2024-06-06 18:56:45 +0600

    Handle guest user view in profile screen

--- app/screens/(tabs)/userProfile.tsx
+++ app/screens/(tabs)/userProfile.tsx
@@ -1,10 +1,12 @@
 import Loader from "@/components/global/Loader";
 import TopBar from "@/components/global/TopBar";
+import { Container, Typography } from "@/components/ui";
 import ProfileDetails from "@/components/user-profile/ProfileDetails";
 import useAuth from "@/hooks/auth/useAuth";
 import { useGetMyProfile } from "@/services/authService";
 import { UserViewModel } from "@tajdid-academy/tajdid-corelib";
-import { View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
 const UserProfile = () => {
   const { token, user } = useAuth();
@@ -14,8 +16,23 @@ const UserProfile = () => {
     <>
       <TopBar />
       {isLoading ? (
-        <View style={{ flex: 1, justifyContent: "center" }}>
+        <View style={styles.container}>
           <Loader />
+        </View>
+      ) : !token ? (
+        <View style={styles.container}>
+          <Container gap={4} style={{ alignItems: "center" }}>
+            <Typography>আপনি এখনো লগিন করেন নি!</Typography>
+            <Typography>
+              দয়া করে{" "}
+              <Link href={"/signIn"}>
+                <Typography weight="semiBold" color="blue500">
+                  লগিন
+                </Typography>
+              </Link>{" "}
+              করুন
+            </Typography>
+          </Container>
         </View>
       ) : (
         <ProfileDetails user={data as UserViewModel} />
@@ -25,3 +42,11 @@ const UserProfile = () => {
 };
 
 export default UserProfile;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
