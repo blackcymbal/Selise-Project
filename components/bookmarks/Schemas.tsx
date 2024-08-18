14a59d6838e295b57a1585e683474f19e9577aba
shahriar <shohan.ndc@gmail.com>
2024-08-19 02:49:54 +0600

    updating

--- components/bookmarks/Schemas.tsx
+++ components/bookmarks/Schemas.tsx
@@ -9,7 +9,7 @@ export const bookmarkSchema = z.object({
     .string()
     .url({ message: "Please enter a valid URL" })
     .min(10, { message: "Url should be at least 10 characters" })
-    .max(30, { message: "Keep your response within 30 characters" }),
+    .max(50, { message: "Keep your response within 50 characters" }),
 });
 
 export type BookmarkSchema = z.infer<typeof bookmarkSchema>;
